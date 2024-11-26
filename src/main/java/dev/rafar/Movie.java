f9fa476d2d05d3db77a02afb022f0aef61120f05
Rafael Roani Gonçalves <rafaelgoncalvesroani@gmail.com>
2024-11-26 20:53:22 -0300

    tenth commit

--- src/main/java/dev/rafar/Movie.java
+++ src/main/java/dev/rafar/Movie.java
@@ -41,4 +41,9 @@ public class Movie {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints(int daysRented){
+        if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+        return 1;
+    }
 }
