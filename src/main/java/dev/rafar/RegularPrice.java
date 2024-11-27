46a66af5d17578d5fe4f2ae1b80182c9ab4a4f35
Rafael Roani Gonçalves <rafaelgoncalvesroani@gmail.com>
2024-11-26 21:12:00 -0300

    thirteenth commit

--- src/main/java/dev/rafar/RegularPrice.java
+++ src/main/java/dev/rafar/RegularPrice.java
@@ -1,7 +1,14 @@
 package dev.rafar;
 
 public class RegularPrice extends Price {
-   public int getPriceCode() {
-       return Movie.REGULAR;
-   }
+    public int getPriceCode() {
+        return Movie.REGULAR;
+    }
+
+    public double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
 }
